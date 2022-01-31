37dd53b8eecabf601811789b04808707558af676
dimfot3 <fotiou.dimitris3@gmail.com>
2022-01-31 23:44:41 +0200

    file system architecture

---
+++ tests/src/utils_test.cpp
@@ -0,0 +1,16 @@
+/**
+ * Author: Fotiou Dimitrios
+ * AEM: 9650
+ * Here there are tests to evaluate correctness of basic utilities
+ **/
+
+#include <gtest/gtest.h>
+#include <utils.h>
+
+
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
