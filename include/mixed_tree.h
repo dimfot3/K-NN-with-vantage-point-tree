6ab9ad78146f2d84f3ec5c94719459ffa95543e3
Andreto-Sama <49563194+Andreto-Sama@users.noreply.github.com>
2022-02-22 13:38:33 +0200

    Add files via upload

---
+++ include/mixed_tree.h
@@ -0,0 +1,14 @@
+/**
+ * Authors: Fotiou Dimitrios(9650), Andreas Eleutheriadis(9649)
+ * @brief Here is the implementation of Vantage point creation with mixed parallel and serial execution
+ **/
+
+/**
+ * @brief this function create vp tree parallel with recursive procedure
+ * 
+ * @param points  the structure for points
+ * @param idxs  the idxs of points for current iteration
+ * @param n the number of points for current itteration
+ * @return return the root point for current itteration
+ **/
+struct vp_point* mixed_vp_create(struct points_struct* points, int* idxs,  int n, struct vp_point* parrent);
