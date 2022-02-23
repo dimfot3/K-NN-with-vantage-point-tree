c0b5f61eb25645608241e6ea1c03c17c0c52806f
dimfot3 <fotiou.dimitris3@gmail.com>
2022-02-23 17:05:20 +0200

    script

--- scripts/find_t_script.sh
+++ scripts/find_t_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for n in {1..50..1}
+for data in dt_2_10000_3000.dat dt_2_10000_3000.dat dt_2_10000_3000.dat
 do
-	for i in {1..20}
+	for n in {1..15..1}
 	do
-    	./main_cpu_program ../data/dt_2_30000_10000.dat $1 $n
-   	done
+		for i in {1..10}
+		do
+			./main_cpu_program ../data/$data $1 $n
+	   	done
+	done
 done
