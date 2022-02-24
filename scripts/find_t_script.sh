9c34766265e95e1e2af38e0306fd77988ec97ec3
dimfot3 <fotiou.dimitris3@gmail.com>
2022-02-24 20:30:12 +0200

    script f

--- scripts/find_t_script.sh
+++ scripts/find_t_script.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-for data in dt_2_10000_3000.dat dt_2_30000_10000.dat dt_2_55000_3000.dat
+for data in dt_2_800_500.dat dt_2_10000_2000.dat dt_2_20000_20000.dat dt_2_30000_5000.dat dt_2_50000_20000.dat
 do
-	for n in {1..15..1}
+	for i in {1..10}
 	do
-		for i in {1..10}
-		do
-			./main_cpu_program ../data/$data $1 $n
-	   	done
-	done
+		mpirun -np 2 main_cpu_program ../data/$data 3 4
+   	done
 done
