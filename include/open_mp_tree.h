e01c3f4654ba3bfc9c09c7960ac567fe43d5b0ea
Andreto-Sama <49563194+Andreto-Sama@users.noreply.github.com>
2022-02-25 18:09:23 +0200

    Update open_mp_tree.h

--- include/open_mp_tree.h
+++ include/open_mp_tree.h
@@ -8,7 +8,7 @@
  * 
  * @param points  the structure for points
  * @param idxs  the idxs of points for current iteration
- * @param n the number of points for current itteration
- * @return return the root point for current itteration
+ * @param n the number of points for current iteration
+ * @return return the root point for current iteration
  **/
 struct vp_point* openmp_vp_create(struct points_struct* points, int* idxs,  int n, struct vp_point* parrent);
